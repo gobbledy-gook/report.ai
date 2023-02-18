22e3d75d8c91f4cd2607969eb332a9e599cb7d73
Garvit Shah <garvit.shah3@gmail.com>
2023-02-18 17:10:39 +0530

    5-Star Rating UI done !

--- rating.js
+++ rating.js
@@ -3,7 +3,7 @@ const btn = document.querySelector("button");
       const widget = document.querySelector(".star-widget");
       const editBtn = document.querySelector(".edit");
       btn.onclick = ()=>{
-        widget.style.display = "none";
+        widget.style.diplay = "none";
         post.style.display = "block";
         editBtn.onclick = ()=>{
           widget.style.display = "block";
