9c4f9d310b72f3fdb97146cc97da0a55a39945ff
AnsahMohammad <mohdansah10@gmail.com>
2023-02-18 17:11:24 +0530

    Finished sorting and sending word cloud success

--- content.js
+++ content.js
@@ -25,5 +25,37 @@ function wordFreq(string) {
 }
 
 var freqMap = wordFreq(theText);
+// console.log(freqMap)
+
+// const sortedFreqMap = Object.entries(freqMap)
+//   .sort(([, val1], [, val2]) => val1 - val2)
+//   .reduce((result, [key, value]) => {
+//     result[key] = value;
+//     return result;
+//   }, {});
+
+// // console.log(sortedFreqMap);
+
+let sorted_Map = [];
+for (var word in freqMap) {
+    sorted_Map.push([word, freqMap[word]]);
+}
+
+sorted_Map.sort(function(a, b) {
+    return b[1] - a[1];
+});
+
+common_words = ['i','-', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+var most_common = [];
+
+sorted_Map.forEach((t) => {
+  if(!common_words.includes(t[0].toLowerCase()) && t[0].length>4){
+    most_common.push(t[0]);
+  }
+});
+
+console.log(most_common);
+
+
 // chrome.runtime.sendMessage(count);
-chrome.storage.local.set({ key: word_count });
+chrome.storage.local.set({ key: most_common });
