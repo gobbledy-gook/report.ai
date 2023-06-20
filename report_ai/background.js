be2887bb171ac9bfc22878a0459b937703a6b394
AnsahMohammad <mohdansah10@gmail.com>
2023-06-20 19:26:03 +0530

    changed alert() to log()

--- report_ai/background.js
+++ report_ai/background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   console.log(activeInfo.tabId);
-  alert("Tab changed");
+  console.log("Tab changed");
   const btn4 = document.querySelector("#refreshBtn");
   btn4.onclick = () =>{
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
