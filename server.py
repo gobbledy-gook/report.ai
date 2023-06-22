9a8e8e4e7ba109434c1ea599029aabc4bd5b8bd7
Garvit Shah <garvitshah@gobbeldy-gook.local>
2023-06-22 12:14:52 +0530

    Python linting issue fix

--- server.py
+++ server.py
@@ -23,9 +23,8 @@ cors = CORS(app)
 
 gpt_neo_key = os.environ.get("GPTNEO", None)
 headers = {"Authorization": gpt_neo_key}
-
-client = MongoClient("mongodb+srv://admin:admin@reportai.ks0reyi.mongodb.net/", 
-        server_api=ServerApi("1"))
+mongo_uri = "mongodb+srv://admin:admin@reportai.ks0reyi.mongodb.net/"
+client = MongoClient(mongo_uri, server_api=ServerApi("1"))
 try:
     client.admin.command("ping")
     print("Pinged your deployment. You successfully connected to MongoDB!")
